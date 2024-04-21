1dbb04117f46a0a4c43c32f33ce1b1e157d8fe7d
Schubert3275 <dragon3275@gmail.com>
2024-04-21 20:27:02 +0900

    Flask Web Franch complete

--- Team4/Bible_Translator/models.py
+++ Team4/Bible_Translator/models.py
@@ -3,21 +3,10 @@ from Bible_Translator import db
 from datetime import datetime
 
 
-## Original 테이블 클래스 -------------------------------------
-## 컬럼 : id, text, create_date
-class Original(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    text = db.Column(db.Text(), nullable=False)
-    create_date = db.Column(db.DateTime(), nullable=False, default=datetime.now())
-
-
 ## Translation 테이블 클래스 ---------------------------------------
-## 컬럼 : id, original_id, original, text, create_date
+## 컬럼 : id, original_text, translation_text, create_date
 class Translation(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    original_id = db.Column(
-        db.Integer, db.ForeignKey("original.id", ondelete="CASCADE")
-    )
-    original = db.relationship("Original", backref=db.backref("translation"))
-    text = db.Column(db.Text(), nullable=False)
+    original_text = db.Column(db.Text(), nullable=False)
+    translation_text = db.Column(db.Text(), nullable=False)
     create_date = db.Column(db.DateTime(), nullable=False, default=datetime.now())
--- Team4/Bible_Translator/views/main_views.py
+++ Team4/Bible_Translator/views/main_views.py
@@ -1,8 +1,55 @@
-from flask import Blueprint
+from flask import Blueprint, render_template, request, redirect
+from ..models import Translation
+from Bible_Translator import db
+from transformers import AutoTokenizer, AutoModelForSeq2SeqLM
+import os
+
 
 bp = Blueprint("main", __name__, url_prefix="/")
 
 
 @bp.route("/")
 def index():
-    return "Welcome to the Bible Translator!"
+    translate_text = Translation.query.order_by(Translation.id.desc()).first()
+    return render_template(
+        "translate.html",
+        translate_language=translate_text,
+    )
+
+
+@bp.route("/translate", methods=["POST"])
+def translate():
+    if request.method == "POST":
+        select_language = request.form["language"]
+        original_text = request.form["Content"]
+        translation_text = translate_langs(select_language, original_text)
+        if original_text and translation_text:
+            t = Translation(
+                original_text=original_text, translation_text=translation_text
+            )
+            db.session.add(t)
+            db.session.commit()
+    return redirect("/")
+
+
+def translate_langs(select_language, original_text):
+    curr_dir = os.getcwd()
+    if select_language == "German":
+        translation_text = ""
+    elif select_language == "Russian":
+        translation_text = ""
+    elif select_language == "French":
+        model_dir = curr_dir + "/Bible_Translator/static/french/results"
+        tokenizer = AutoTokenizer.from_pretrained(model_dir)
+        model = AutoModelForSeq2SeqLM.from_pretrained(model_dir)
+        inputs = tokenizer(original_text, return_tensors="pt", padding=True)
+        frenchs = model.generate(
+            **inputs,
+            max_length=128,
+            num_beams=5,
+        )
+        translation_text = tokenizer.batch_decode(frenchs, skip_special_tokens=True)[0]
+    else:
+        translation_text = ""
+
+    return translation_text
